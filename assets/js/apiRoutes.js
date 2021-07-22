0bc4a0ede4dd34d5dfd30ed1ddb7d2ef0a7cfa4d
Jose Rosas <josejrrosas@yahoo.com>
2021-07-21 22:18:07 -0500

    redid new notes post and created delete to delete notes

--- assets/js/apiRoutes.js
+++ assets/js/apiRoutes.js
@@ -1,43 +1,58 @@
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-const path = require('path');
-const notesData = require(path.join(__dirname, '../../db/db.json'));
-const fs = require("fs");
+const fs = require('fs');
 
-// ROUTING
+module.exports = function(app) {
 
-module.exports = (app) => {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get('/api/notes', (req, res) => res.json(notesData));
-  app.get('/api/notes:id'), (req,res) => res.json(notesData[Number(req.params.id)]);
-
-  // API POST Requests
-  // Below code handles when a user submits a note and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a notes request... this data is then sent to the server...
-  // Then the server saves the data to the db.json array)
-  // ---------------------------------------------------------------------------
-
-  app.post('/api/notes', (req, res) => {
-    // req.body is available since we're using the body parsing middleware
-        let newNote = req.body;
-        let uniqueId = (notesData.length).toString();
-        console.log(uniqueId);
-        newNote.id = uniqueId;
-        notesData.push(newNote);
-        
-        fs.writeFileSync("./db/db.json", JSON.stringify(notesData), function(err) {
-            if (err) throw (err);        
-        }); 
-
-        res.json(notesData);  
+  app.get('/api/notes', function(req, res) {
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      res.send(dbData);
+    });
   });
 
+  // post new note to db.json 
+  app.post('/api/notes', function(req, res) {
+    const userNotes = req.body;
+
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      dbData.push(userNotes);
+      let number = 1;
+      dbData.forEach((note, index) => {
+        note.id = number;
+        number++;
+        return dbData;
+      });
+      console.log(dbData);
+
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.send('Thank you for your note!');
+  });
+
+  // delete note
+  app.delete('/api/notes/:id', function(req, res) {
+    const deleteNote = req.params.id;
+
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+
+      noteData = JSON.parse(data);
+      for (let i = 0; i < noteData.length; i++) {
+        if (noteData[i].id === Number(deleteNote)) {
+          noteData.splice([i], 1);
+        }
+      }
+      stringData = JSON.stringify(noteData);
+
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+  });
 };
